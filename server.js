411d70d3ecde9cb3568a204cb928fd3b9f3a5676
Anuj <161225144+Backlinko2@users.noreply.github.com>
2025-08-06 21:37:16 +0530

    Added root route for base URL

--- server.js
+++ server.js
@@ -18,5 +18,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
+app.get('/', (req, res) => {
+    res.send('Backend is running successfully!');
+  });
+  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
